9235525ab4cabd89952fff2cfcb384d5d997a725
MarceloMikael <marcelo.mplp@hotmail.com>
2024-08-31 22:05:14 -0300

    Updated and installed new configs.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
+opt.completeopt = "menu,menuone,noselect"
+opt.cursorline = true -- Enable highlighting of the current line
+opt.smartindent = true -- Insert indents automatically
+opt.wrap = true -- Disable line wrap
